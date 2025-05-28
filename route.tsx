9572291af5c2991b244e4b5d104ebd7b1a0685cc
karthickrajan S <93571352+Karthick1242004@users.noreply.github.com>
2025-05-28 23:39:54 +0530

    Update route.tsx

--- route.tsx
+++ route.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate, createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter, useParams } from "react-router-dom";
 import { AuthenticatedTemplate } from "@azure/msal-react";
 import Layout from "./shared/layout/layout";
 
@@ -42,8 +42,11 @@ const routes = [
             lazy: async () => {
               const { default: WorkspaceEdit } = await import("./modules/workspace/dynamic-form/components/WorkspaceEdit");
               const { formConfigs } = await import("./modules/workspace/dynamic-form/formtypes");
-              const Component = (props: any) => {
-                const id = props.params.id;
+              const Component = () => {
+                const { id } = useParams();
+                if (!id) {
+                  return <Navigate to="/workspace" />;
+                }
                 return <WorkspaceEdit id={id} config={formConfigs.workspace} initialData={{}} />;
               };
               return { Component };
@@ -54,8 +57,11 @@ const routes = [
             lazy: async () => {
               const { default: SkillEdit } = await import("./modules/workspace/dynamic-form/components/SkillEdit");
               const { formConfigs } = await import("./modules/workspace/dynamic-form/formtypes");
-              const Component = (props: any) => {
-                const id = props.params.id;
+              const Component = () => {
+                const { id } = useParams();
+                if (!id) {
+                  return <Navigate to="/workspace" />;
+                }
                 return <SkillEdit id={id} config={formConfigs.skill} initialData={{}} />;
               };
               return { Component };
