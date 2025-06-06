80ca3643cd1cd9ac2416eebafb85b406ad14fc6a
karthickrajan S <93571352+Karthick1242004@users.noreply.github.com>
2025-06-06 12:40:46 +0530

    Update DropdownMenuCheckboxes.tsx

--- DropdownMenuCheckboxes.tsx
+++ DropdownMenuCheckboxes.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { AArrowDown, ArrowDown01, ArrowUp01, Calendar1, ChevronDown, CheckCircle2, XCircle, Clock } from "lucide-react"
+import { AArrowDown, ArrowDown01, ArrowUp01, Calendar1, ChevronDown, CheckCircle2, XCircle, Clock, GalleryVerticalEnd, CheckCheck, Hourglass, ListFilter } from "lucide-react"
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
@@ -16,14 +16,16 @@ interface DropdownMenuCheckboxesProps<T extends string> {
 
 export function DropdownMenuCheckboxes<T extends string>({ onSort, currentSort, isWorkspace = false }: DropdownMenuCheckboxesProps<T>) {
   const workspaceSortOptions = [
-    { id: "alphabetical", label: "Alphabetical", icon: <AArrowDown /> },
-    { id: "lastModified", label: "Last Modified", icon: <Calendar1 /> },
+    // { id: "A-Z", label: "Alphabetical", icon: <AArrowDown /> },
+    { id: "A-Z", label: "A-Z", icon: <ArrowUp01 /> },
+    { id: "Z-A", label: "Z-A", icon: <ArrowDown01 /> },
   ]
 
   const statusFilterOptions = [
-    { id: "completed", label: "Completed", icon: <CheckCircle2 /> },
-    { id: "failed", label: "Failed", icon: <XCircle /> },
-    { id: "inprogress", label: "In Progress", icon: <Clock /> },
+    { id: "All", label: "All", icon: <GalleryVerticalEnd /> },
+    { id: "Completed", label: "Completed", icon: <CheckCheck /> },
+    { id: "Inprogress", label: "In Progress", icon: <Hourglass /> },
+    { id: "Failed", label: "Failed", icon: <XCircle /> },
   ]
 
   const options = isWorkspace ? workspaceSortOptions : statusFilterOptions;
@@ -35,57 +37,30 @@ export function DropdownMenuCheckboxes<T extends string>({ onSort, currentSort, 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <button className="flex items-center text-xs gap-2 px-4 h-9 font-medium text-blue-600 bg-blue-100 rounded-xl hover:bg-blue-200">
-          {isWorkspace ? "Sort" : "Filter"}
+        <button className="flex items-center text-xs gap-2 px-4 h-9 font-medium text-blue-600 bg-blue-100 rounded-sm hover:bg-blue-200">
+        <ListFilter size={15} className="text-blue-600" />
+          {currentSort}
           <ChevronDown className="w-4 h-4" />
         </button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-50 bg-blue-100 rounded-xl shadow-lg border-[var(--workspace-color-highlight)]">
+      <DropdownMenuContent className="w-fit mr-7 bg-white rounded-sm shadow-lg border-none">
         {options.map((option, index) => (
           <React.Fragment key={option.id}>
             <DropdownMenuCheckboxItem
               checked={currentSort === option.id}
               onCheckedChange={() => handleSortChange(option.id)}
-              className="flex items-center px-4 py-2 text-sm cursor-pointer hover:bg-gray-50"
+              className="flex font-unilever items-center px-4 py-2 text-sm cursor-pointer hover:bg-blue-100"
             >
               <span className="flex items-center">
-                <span className="w-10 ml-3 text-center text-xs">{option.icon}</span>
+                {/* <span className="w-10 ml-3 text-center text-xs">{option.icon}</span> */}
                 <span className="text-xs">{option.label}</span>
               </span>
             </DropdownMenuCheckboxItem>
             {index < options.length - 1 && (
               <DropdownMenuSeparator className="my-1 border-gray-100" />
             )}
-            {isWorkspace && index === workspaceSortOptions.length - 1 && (
-              <DropdownMenuSeparator className="my-1 border-gray-100" />
-            )}
           </React.Fragment>
         ))}
-        {isWorkspace && (
-          <>
-            <DropdownMenuCheckboxItem
-              checked={currentSort === "ascending"}
-              onCheckedChange={() => handleSortChange("ascending")}
-              className="flex items-center px-4 py-2 text-sm cursor-pointer hover:bg-gray-50"
-            >
-              <span className="flex items-center">
-                <span className="w-10 ml-3 text-center text-xs"><ArrowUp01 /></span>
-                <span className="text-xs">Ascending</span>
-              </span>
-            </DropdownMenuCheckboxItem>
-            <DropdownMenuSeparator className="my-1 border-gray-100" />
-            <DropdownMenuCheckboxItem
-              checked={currentSort === "descending"}
-              onCheckedChange={() => handleSortChange("descending")}
-              className="flex items-center px-4 py-2 text-sm cursor-pointer hover:bg-gray-50"
-            >
-              <span className="flex items-center">
-                <span className="w-10 ml-3 text-center text-xs"><ArrowDown01 /></span>
-                <span className="text-xs">Descending</span>
-              </span>
-            </DropdownMenuCheckboxItem>
-          </>
-        )}
       </DropdownMenuContent>
     </DropdownMenu>
   )
