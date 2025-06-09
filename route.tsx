6e3aff4b9ae8aa6d79d0ec800fe70b8a8226aca7
karthickrajan S <93571352+Karthick1242004@users.noreply.github.com>
2025-06-09 16:42:46 +0530

    Update route.tsx

--- route.tsx
+++ route.tsx
@@ -1,17 +1,13 @@
-case "multiselect":
-      return (
-        <Controller
-          name={field.name}
-          control={control}
-          rules={{ required: field.required ? `${field.label} is required` : false }}
-          render={({ field: { value, onChange } }) => (
-            <MultipleSelector
-              value={value}
-              onChange={onChange}
-              options={field.options?.map(opt => ({ value: opt, label: opt })) || []}
-              placeholder={`Select ${field.label}`}
-              className="bg-white border-gray-200 !text-xs"
-            />
-          )}
-        />
-      );
+multiselect.tsx:589 Encountered two children with the same key, `[object Object]`. Keys should be unique so that components maintain their identity across updates. Non-unique keys may cause children to be duplicated and/or omitted — the behavior is unsupported and could change in a future version.
+
+react-router-dom.js?v=cc108d0e:5397 Error handled by React Router default ErrorBoundary: Error: Objects are not valid as a React child (found: object with keys {lable, value}). If you meant to render a collection of children, use an array instead.
+    at throwOnInvalidObjectType (react-dom_client.js?v=cc108d0e:5440:15)
+    at reconcileChildFibersImpl (react-dom_client.js?v=cc108d0e:6041:13)
+    at react-dom_client.js?v=cc108d0e:6061:35
+    at reconcileChildren (react-dom_client.js?v=cc108d0e:6444:53)
+    at beginWork (react-dom_client.js?v=cc108d0e:7830:104)
+    at runWithFiberInDEV (react-dom_client.js?v=cc108d0e:1485:72)
+    at performUnitOfWork (react-dom_client.js?v=cc108d0e:10868:98)
+    at workLoopSync (react-dom_client.js?v=cc108d0e:10728:43)
+    at renderRootSync (react-dom_client.js?v=cc108d0e:10711:13)
+    at performWorkOnRoot (react-dom_client.js?v=cc108d0e:10359:46)
