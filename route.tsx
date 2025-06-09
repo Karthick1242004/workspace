92d4008efa22e8f96564d5d13ae785a6510e40f6
karthickrajan S <93571352+Karthick1242004@users.noreply.github.com>
2025-06-09 17:20:35 +0530

    Update route.tsx

--- route.tsx
+++ route.tsx
@@ -1,13 +1,41 @@
-multiselect.tsx:589 Encountered two children with the same key, `[object Object]`. Keys should be unique so that components maintain their identity across updates. Non-unique keys may cause children to be duplicated and/or omitted — the behavior is unsupported and could change in a future version.
-
-react-router-dom.js?v=cc108d0e:5397 Error handled by React Router default ErrorBoundary: Error: Objects are not valid as a React child (found: object with keys {lable, value}). If you meant to render a collection of children, use an array instead.
-    at throwOnInvalidObjectType (react-dom_client.js?v=cc108d0e:5440:15)
-    at reconcileChildFibersImpl (react-dom_client.js?v=cc108d0e:6041:13)
-    at react-dom_client.js?v=cc108d0e:6061:35
-    at reconcileChildren (react-dom_client.js?v=cc108d0e:6444:53)
-    at beginWork (react-dom_client.js?v=cc108d0e:7830:104)
-    at runWithFiberInDEV (react-dom_client.js?v=cc108d0e:1485:72)
-    at performUnitOfWork (react-dom_client.js?v=cc108d0e:10868:98)
-    at workLoopSync (react-dom_client.js?v=cc108d0e:10728:43)
-    at renderRootSync (react-dom_client.js?v=cc108d0e:10711:13)
-    at performWorkOnRoot (react-dom_client.js?v=cc108d0e:10359:46)
+{
+    "data": [
+        {
+            "id": 1,
+            "name": "GPT-4o",
+            "description": "GPT-4.1 is a large multimodal language model developed by OpenAI, capable of understanding and generating natural language and code.",
+            "is_active": true,
+            "supported_file_formats": []
+        },
+        {
+            "id": 5,
+            "name": "GPT-4o-mini",
+            "description": "GPT-4o-mini",
+            "is_active": true,
+            "supported_file_formats": [
+                "png",
+                "jpg",
+                "jpeg"
+            ]
+        },
+        {
+            "id": 6,
+            "name": "Gemini-1.5-flash",
+            "description": "Gemini-1.5-flash",
+            "is_active": true,
+            "supported_file_formats": []
+        },
+        {
+            "id": 7,
+            "name": "Gemini-Pro",
+            "description": "Gemini-Pro",
+            "is_active": true,
+            "supported_file_formats": []
+        }
+    ],
+    "message": "Success",
+    "sql_info": {
+        "query": "SELECT [ai_models].[id], [ai_models].[name], [ai_models].[description], [ai_models].[is_active], [ai_models].[supported_file_formats] FROM [ai_models] WHERE [ai_models].[is_active] = True",
+        "execution_time": 1.7435996532440186
+    }
+}
