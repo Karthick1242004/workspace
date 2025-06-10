f24572316b2be936f73d0024ef3bb9e1d74af6a9
karthickrajan S <93571352+Karthick1242004@users.noreply.github.com>
2025-06-10 17:54:51 +0530

    Update workspace-card.tsx

--- workspace-card.tsx
+++ workspace-card.tsx
@@ -84,10 +84,11 @@ export default function WorkspaceCard({ data, isSelected }: Props) {
 
   return (
     <div
-      className={`group border rounded-lg p-4 relative transition-all duration-300 hover:border-[var(--workspace-color-highlight)] hover:shadow-[0_0_10px_rgba(96,165,250,0.5)] ${isSelected
-        ? "border-[var(--workspace-color-highlight)] bg-[var(--workspace-color-highlight)] text-white"
-        : "bg-white border-[#b5d3ff] text-gray-600"
-        }`}
+      className={`group border rounded-lg p-4 relative transition-all duration-300 h-[200px] hover:border-[var(--workspace-color-highlight)] hover:shadow-[0_0_10px_rgba(96,165,250,0.5)] ${
+        isSelected
+          ? "border-[var(--workspace-color-highlight)] bg-[var(--workspace-color-highlight)] text-white"
+          : "bg-white border-[#b5d3ff] text-gray-600"
+      }`}
       style={{
         backgroundImage: 'url("@/assets/icons/Frame-2106257085-copy.svg")',
         backgroundRepeat: "no-repeat",
@@ -96,72 +97,64 @@ export default function WorkspaceCard({ data, isSelected }: Props) {
         backgroundSize: "100px",
       }}
     >
-      {/* {(data.access_level === "owner" || data.access_level === "editor") &&
-        <button
-          onClick={(e) => { e.stopPropagation(), navigate(`/workspace/edit/${data.id}`, { state: { workspace: data } }) }}
-          className="absolute top-0 right-2 p-1.5 rounded-full text-xs text-[var(--workspace-color-highlight)]"
-          aria-label="Edit workspace"
-        >
-          <span className="inline-block cursor-pointer opacity-0 bg-white border p-1 rounded-sm z-10 border-gray-200 translate-x-2 transition-all duration-300 ease-out group-hover:opacity-100 group-hover:translate-x-0">
-            <PencilLine size={16} />
-          </span>
-        </button>
-       } */}
-        {(data.access_level === "owner" || data.access_level === "editor") && 
-        <div className=" mt-[-0.5rem] flex flex-row  rounded-sm">
-        <Tooltip>
-          <TooltipTrigger>
-            <button
-               onClick={(e) => { e.stopPropagation(), navigate(`/workspace/edit/${data.id}`, { state: { workspace: data } }) }}
-              className="bg-white rounded-l-sm border-t border-l border-b border-blue-200 p-1 text-xs text-[var(--workspace-color-highlight)] opacity-0 group-hover:opacity-100 transition cursor-pointer"
-              aria-label="Edit skill"
-            >
-              <PencilLine size={16} />
-            </button>
-          </TooltipTrigger>
-          <TooltipContent side="bottom" sideOffset={2} disableArrow>
-            <p>Edit</p>
-          </TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger>
-            <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-              <DialogTrigger asChild>
-                <button
-                  onClick={(e) => e.stopPropagation()}
-                  className="bg-white rounded-r-sm border-b border-t border-r border-blue-200 p-1 text-xs text-red-500 opacity-0 group-hover:opacity-100 transition cursor-pointer"
-                  aria-label="Edit skill"
-                >
-                  <Trash2 size={16} />
-                </button>
-              </DialogTrigger>
-
-              <DialogContent
-                className="sm:max-w-[480px] bg-white"
+      {(data.access_level === "owner" || data.access_level === "editor") && (
+        <div className="absolute right-4 mt-[-0.5rem] flex flex-row rounded-sm border border-blue-200 bg-white opacity-0 group-hover:opacity-100 transition">
+          <Tooltip>
+            <TooltipTrigger>
+              <button
                 onClick={(e) => {
                   e.stopPropagation();
+                  navigate(`/workspace/edit/${data.id}`, { state: { workspace: data } });
                 }}
+                className="p-1 text-xs text-[var(--workspace-color-highlight)] cursor-pointer rounded-none"
+                aria-label="Edit workspace"
               >
-                <DeleteDialog
-                  setIsDialogOpen={setIsDialogOpen}
-                  handleDelete={handleDelete}
-                  itemType="workspace"
-                  itemName={data.name || "this workspace"}
-                  id={data.id}
-                  isSkill={isSkill}
-                />
-              </DialogContent>
-            </Dialog>
-          </TooltipTrigger>
+                <PencilLine size={16} />
+              </button>
+            </TooltipTrigger>
+            <TooltipContent side="bottom" sideOffset={2} disableArrow>
+              <p>Edit</p>
+            </TooltipContent>
+          </Tooltip>
+          {data.access_level === "owner" && (
+            <Tooltip>
+              <TooltipTrigger>
+                <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+                  <DialogTrigger asChild>
+                    <button
+                      onClick={(e) => e.stopPropagation()}
+                      className="p-1 text-xs text-red-500 cursor-pointer rounded-none"
+                      aria-label="Delete workspace"
+                    >
+                      <Trash2 size={16} />
+                    </button>
+                  </DialogTrigger>
 
-          <TooltipContent side="bottom" sideOffset={2} disableArrow>
-            <p>Delete</p>
-          </TooltipContent>
-        </Tooltip>
-      </div>
-        }
-      <div className="flex gap-4 items-start justify-between">
-        <div className="flex-1 min-w-0">
+                  <DialogContent
+                    className="sm:max-w-[480px] bg-white"
+                    onClick={(e) => e.stopPropagation()}
+                  >
+                    <DeleteDialog
+                      setIsDialogOpen={setIsDialogOpen}
+                      handleDelete={handleDelete}
+                      itemType="workspace"
+                      itemName={data.name || "this workspace"}
+                      id={data.id}
+                      isSkill={isSkill}
+                    />
+                  </DialogContent>
+                </Dialog>
+              </TooltipTrigger>
+              <TooltipContent side="bottom" sideOffset={2} disableArrow>
+                <p>Delete</p>
+              </TooltipContent>
+            </Tooltip>
+          )}
+        </div>
+      )}
+
+      <div className="flex gap-4 items-start justify-between h-full">
+        <div className="flex-1 min-w-0 flex flex-col">
           <div className="flex items-center gap-2 mb-3">
             <h2
               className={`font-unilever-medium text-base truncate ${isSelected
@@ -219,10 +212,12 @@ export default function WorkspaceCard({ data, isSelected }: Props) {
               {data.skills?.length ?? 0} Skills
             </div>
           </div>
-          <p className="text-xs font-unilever line-clamp-3">
+
+          <p className="text-xs font-unilever flex-grow line-clamp-3 overflow-hidden max-h-[4.6em] leading-[1.5em]">
             {data.description}
           </p>
         </div>
+
         {/* Logo or Skeleton */}
         <div className="flex-shrink-0 w-[80px] h-[80px] flex items-center justify-center border border-[#eaeff5] rounded-sm bg-white ml-2">
           {isLoading ? (
